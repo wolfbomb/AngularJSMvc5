32bb83044802a19596063349ed10b5086cdfb47e
wolfbomb <nimrod.resulta@gmail.com>
2017-08-15 05:52:46 +0800

    Initia commit

---
+++ AngularJSMvc5/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AngularJSMvc5
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AngularJSMvc5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularJSMvc5.MvcApplication" Language="C#" %>
